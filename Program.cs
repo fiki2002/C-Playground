c17161b2e5c7f45e3cfef381329f69b5c86f4f73
fiki2002 <adepitanoluwatosin202@gmail.com>
2024-08-24 13:32:50 +0100

    Rank your Grade init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("******* Welcome to RankYourGrade *******");
+Console.WriteLine(" ");
+Console.WriteLine("Input your score to get a grade!");
+
+string? scoreInputted = Console.ReadLine();
+
+        if (int.TryParse(scoreInputted, out int scoreInInteger))
+        {
+            string grade;
+
+            if (scoreInInteger >= 70 && scoreInInteger <= 100)
+            {
+                grade = "A";
+            }
+            else if (scoreInInteger >= 60 && scoreInInteger <= 69)
+            {
+                grade = "B";
+            }
+            else if (scoreInInteger >= 50 && scoreInInteger <= 59)
+            {
+                grade = "C";
+            }
+            else if (scoreInInteger >= 40 && scoreInInteger <= 49)
+            {
+                grade = "D";
+            }
+            else if (scoreInInteger >= 0 && scoreInInteger <= 39)
+            {
+                grade = "F";
+            }
+            else
+            {
+                Console.WriteLine($"{scoreInInteger} was invalid, accepted range is 0 - 100");
+                return;
+            }
+
+            Console.WriteLine($"Oh, you had {scoreInInteger}");
+            Console.Write("Your grade is: ");
+            Console.WriteLine(grade);
+        }
+        else
+        {
+            Console.WriteLine($"{scoreInputted} is not a valid number.");
+        }
